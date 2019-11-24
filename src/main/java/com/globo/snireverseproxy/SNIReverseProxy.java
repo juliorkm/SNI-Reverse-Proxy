8c81feaf4a56a1a26369efad5739758f69c883bb
juliorkm <julioramakrsnamandoju@gmail.com>
2019-11-24 00:31:56 -0300

    Initial commit

---
+++ src/main/java/com/globo/snireverseproxy/SNIReverseProxy.java
@@ -0,0 +1,11 @@
+package com.globo.snireverseproxy;
+
+/**
+ * Hello world!
+ *
+ */
+public class SNIReverseProxy {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
